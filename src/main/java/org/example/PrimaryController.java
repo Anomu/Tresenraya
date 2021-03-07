921c04b76f5e5cc0c8b529e276a58f6876ebd07f
Anomu <anomu_xz@hotmail.com>
2021-03-08 00:03:47 +0100

    -

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -157,7 +157,7 @@ public class PrimaryController {
         }else if(grid [0][0] != 0 && grid [1][1] == grid [0][0] && grid [2][2] == grid [0][0]){
 
         }else if(grid [0][2] != 0 && grid [1][1] == grid [0][2] && grid [2][0] == grid [0][2]){
-
+            System.out.println("lololo");
         }
         return winner;
     }
